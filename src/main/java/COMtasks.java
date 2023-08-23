0036398b48f551bd91106e4c62ac75df82ad8516
Urho Saari <urkkiz225@gmail.com>
2023-08-23 23:42:15 +0300

    added parameter reading and user-inputted values for executing methods

---
+++ src/main/java/COMtasks.java
@@ -0,0 +1,64 @@
+import org.w3c.dom.Node;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Random;
+
+public class COMtasks {
+    public static void compare(int a, int b){
+        System.out.println(a + "" + b);
+        boolean isIt = true, really = false;
+        Boolean positive = a > 0;
+        double coeff=1.24;
+        System.out.println(positive + ":" + (float)coeff*random.nextInt(a,b));
+        Node node1 = null;
+        Node node2 = null;
+        LinkedList list = new LinkedList();
+        list.add(node1);
+        list.add(node2);
+
+    }
+    public static void Largest(int largest, int smallest, int loopLim) {
+        for (int i = 0; i < loopLim; i++) {
+            int val = i * (i - 36) * (i - loopLim);
+            if (val < largest) {
+                largest = val;
+            }
+            if (val < smallest) {
+                smallest = val;
+            }
+        }
+        System.out.println(smallest + "," + largest);
+        float Step = 0.1f;
+        float S = 100000;
+        float n = 0;
+        System.out.print("\ndoes equate to smaller than S with n being " + SmallerThanS(n, S, Step));
+    }
+
+
+    public static void blackjack(int cardValues){
+        ArrayList<Object[]> ValueMap = new ArrayList<>(){
+            {
+                add(new Object[]{21, 31, "Bust! "});
+                add(new Object[]{17,21, "Stay! "});
+                add(new Object[]{2, 17, "Hit! "});
+            }
+        };
+        for (Object[] map:ValueMap) {
+            if((int)map[0]<=cardValues&&(int)map[1]>cardValues){
+                System.out.print((String)map[2]+cardValues);
+                break;
+            }
+        }
+    }
+    public static void CalcBacteria(){
+        System.out.println("bruh!!1!");
+    }
+
+    //util
+    public static Random random = new Random();
+    protected static float SmallerThanS(float n, float S, float Step){
+        for(; (n*(n+1)/2)<S; n=Math.round((n+Step)/Step)*Step) System.out.print("\rdoes not equate to smaller than S with n being " + n);
+        return n;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,90 @@
-import java.util.Random;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+    public static Method CurrentMethod;
     public static void main(String[] args){
-        LargestIn.Largest(0,0, 50);
-        int a=1;
-        int b=1;
-        System.out.println((a+b));
-        Compare.compare(Compare.random.nextInt(4,12), Compare.random.nextInt(0,Integer.MAX_VALUE));
-        BlackJack.blackjack(Compare.random.nextInt(2,31));
+        Method[] methods = COMtasks.class.getDeclaredMethods();
+        ArrayList<String> methodNames = new ArrayList<>();
+        for (Method method : methods) {
+            methodNames.add("\n{"+method.getName()+", "+Arrays.stream(methods).toList().indexOf(method)+"}");
+        }
+        try {
+            executeMethodByName(COMtasks.class, userInput("Please type in the name or index of the method that you want to execute from the following list: \n " + methodNames + "\n"));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public static void executeMethodByName(Class<?> targetClass, String methodIdentifier) {
+        Method[] methods=targetClass.getDeclaredMethods();
+        ArrayList<Object> params = new ArrayList<>();
+        for(Method method : methods) {
+            try{
+                if (method.getName().equals(methodIdentifier)) {
+                    CurrentMethod = method;
+                } else if (Arrays.stream(methods).toList().contains(methods[Integer.parseInt(methodIdentifier)])) {
+                    CurrentMethod = methods[Integer.parseInt(methodIdentifier)];
+                }
+            }catch (NumberFormatException e){
+                ynInput("Invalid index or no such method exists in the class " + targetClass + ", try again? y/n");
+            }
+            try {
+                for(Parameter ignored : CurrentMethod.getParameters()) {
+                    try {
+                        params.add(Integer.valueOf(userInput("Please enter valid argument at parameter index " + (params.size()) + " with a type of " +
+                                (CurrentMethod.getParameterTypes()[params.size()])+" and with a name of:"+(CurrentMethod.getParameters()[params.size()].getName()))));
+                    }catch(Exception e){
+                        System.out.println(e);
+                        main(new String[]{});
+                    }
+                }
+                System.out.println(params);
+                CurrentMethod.invoke(targetClass.getDeclaredConstructor().newInstance(), params.stream().toArray());
+                break;
+            }
+            catch(NoSuchMethodException e) {
+                ynInput("No such method exists in the class " + targetClass + ", try again? y/n");
+            } catch(NullPointerException e){
+                ynInput("No such method exists in the class " + targetClass + ", try again? y/n");
+                System.out.print(e);
+            } catch(Exception e) {
+                System.out.println("Something went wrong!"+ e);
+                main(new String[]{});
+            }
+        }
+    }
+    public static boolean ynInput(String message) {
+        System.out.println(message);
+        String userInputS=userInput("Please type value and press enter");
+        if(userInputS.equals("y")||userInputS.equals("yes")) {
+            try {
+                main(new String[]{});
+            }catch (Exception e){
+                System.out.println(e);
+            }
+        }else if(userInputS.equals("n")||userInputS.equals("no")){
+            userInput("\n");
+
+        }else {
+            System.out.println("Non-correct syntax, answer y/n|yes/no (case sensitive)");
+            ynInput(message);
+        }
+        return false;
+    }
+    public static String userInput(String message) {
+        System.out.print(message + "\n");
+        Scanner scanner = new Scanner(System.in);
+        String nextLn = scanner.nextLine();
+        switch (nextLn.toUpperCase()) {
+            case "/EXIT", "/RAND" -> {
+                if(ynInput("Exit?"))
+                    System.exit(0);
+            }
+        }
+        scanner.close();
+        return nextLn;
     }
 }
